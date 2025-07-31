9150eacd2999fcc53412b28e735401078722cf3a
CaduDevWeb <carloseduardo.casotti@outlook.com>
2025-07-31 12:17:45 -0300

    Html e Css finalizado

---
+++ script.js
@@ -0,0 +1,18 @@
+timer = document.getElementById("Timer__totime")
+buttonStart = document.getElementById("organizador-btn__iniciar-btn")
+buttonPause = document.getElementById("organizador-btn__pausa-btn")
+buttonRestart = document.getElementById("organizador-btn__reiniciar-btn")
+
+function initializeApp(){
+    let totalTime = 25;
+    let isPause = false;
+    const actuallySection = null;
+    updateDisplay()
+}
+
+function updateDisplay(){
+    
+}
+
+initializeApp()
+
