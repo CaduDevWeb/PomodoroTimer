fc226eba26bb2a6c160dbb625873ba02495e4e63
CaduDevWeb <carloseduardo.casotti@outlook.com>
2025-08-13 17:41:13 -0300

    inicio da construcao de logica de tempo pomodoro

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const buttonRestart = document.getElementById("organizador-btn__reiniciar-btn")
 let minutes = 0;
 let seconds = 10;
 let idForInterval = null;
-
+let cont = 0;
+const timeline = [25, 5, 25, 5, 25, 5, 25, 30];
 function pauseTime() {
     clearInterval(idForInterval);
     idForInterval = null;
@@ -16,12 +17,21 @@ function restartTime() {
     minutes = 1;
     seconds = 0;
 }
+function breackTime() {
+    //testar se a logica do breckTime esta funcionando e adicionar visual novo quando entrar breackTime 
+    cont ++
+    minutes = timeline[cont];
+    if(cont > timeline.length){
+        cont = 0;
+        minutes = timeline[cont];
+    }
+}
 
 function startTime() {
     if (idForInterval === null) {
         idForInterval = setInterval(() => {
             if (minutes === 0 && seconds === 0) {
-                pauseTime();
+                breackTime();
             } else {
                 if (seconds === 0) {
                     minutes--;
@@ -44,6 +54,6 @@ function updateDisplay(minutes, seconds) {
 buttonPause.addEventListener('click', pauseTime);
 buttonStart.addEventListener('click', startTime);
 buttonRestart.addEventListener('click', restartTime);
-startTime();
+//startTime();
 
 
